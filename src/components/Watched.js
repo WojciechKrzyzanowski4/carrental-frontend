4221a099b2f3a7de140be3f1e2a82401c53fba57
Mikolaj Frycz <mikolajfrycz@gmail.com>
2024-03-21 23:06:45 +0100

    Watched section get request.

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -1,16 +1,53 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from "react";
+import { SERVER_URL } from "./utilComponents/constant";
 
 const Watched = () =>{
+    const [offers, setOffers] = useState([]);
 
+    const getOffers = async () => {
+        try {
+            const response = await fetch(
+                SERVER_URL + '/user/liked-offers',
+                { method: 'GET', redirect: "follow", credentials:'include' }
+            );
+            if (response.redirected) {
+                document.location = response.url;
+            }
+            const data = await response.json();
+            setOffers(data);
+        } catch (error) {
+            console.error('Error fetching offers:', error);
+        }
+    };
 
     useEffect(()=>{
         document.body.style.overflow = 'visible';
-       
+        getOffers();
     }, []);
 
     return(
         <div className="pt-16">
-            hhhhh
+            {offers.map(offer => (
+            <div>
+                <h1>
+                    {offer.id}
+                </h1>
+                <h1>
+                    {offer.name}
+                </h1>
+                <h1>
+                    {offer.brand}
+                </h1>
+                <h1>
+                    {offer.pric}
+                </h1>
+                <h1>
+                    {offer.description}
+                </h1>
+            </div>
+            ))
+
+            }
         </div>
     );
 }
