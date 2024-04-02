11120f4af8dfaa335039e3d53cf97f7418406db7
WojciechKrzyzanowski4 <spider.wojciech@gmail.com>
2024-04-02 09:44:04 +0200

    added new alert and dislike

--- src/components/Watched.js
+++ src/components/Watched.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "./utilComponents/constant";
 import { IoHeartDislike } from "react-icons/io5";
 import Offer from "./offer/Offer";
+import Alert from "./utilComponents/Alert";
 
 const Watched = () =>{
 
     const [offers, setOffers] = useState([]);
+    const [showAlert, setShowAlert] = useState(false);
+
 
     const getOffers = async () => {
         try {
@@ -23,6 +26,31 @@ const Watched = () =>{
         }
     };
 
+    const dislikeOffer = async (id) => {
+        try {
+            const response = await fetch(
+                SERVER_URL + '/offer/' + id + '/dislike',
+                { method: 'GET', redirect: "follow", credentials:'include' }
+            );
+            if (response.ok) {
+                console.log("Offer disliked successfully");
+            } else {
+                console.error("Error occurred while liking offer");
+            }
+        } catch (error) {
+            console.error('Error liking offer:', error);
+        }
+    };
+
+    const handleShowAlert = async (id) => {
+        await dislikeOffer(id);
+        getOffers();
+        setShowAlert(true);
+        setTimeout(() => {
+            setShowAlert(false);
+        }, 5000); 
+    };
+
     useEffect(()=>{
         document.body.style.overflow = 'visible';
         getOffers();
@@ -30,14 +58,12 @@ const Watched = () =>{
 
     return(
         <div className="pt-16">
-        
-        
-        <div className="grid grid-cols-1 gap-6 m-4 justify-center items-center pt-20">
+            <div className="grid grid-cols-1 gap-6 m-4 justify-center items-center pt-20">
             {offers.map((offer) => (
                 <div key={offer.id} className="relative flex flex-col items-center justify-center rounded-xl bg-center bg-[#253237] bg-overflow-hidden shadow-lg">
                     <div className="w-full h-80 bg-cover bg-center" /*this is here is somebody wants to add images*/ style={{ backgroundImage: `url(${offer.imageUrl})` }}>
                         <div className="absolute top-8 right-8">
-                            <button className="rounded-full bg-[#e0fbfc] p-2 text-gray-500 hover:text-black">
+                            <button onClick={() => handleShowAlert(offer.id)} className="rounded-full bg-[#e0fbfc] p-2 text-gray-500 hover:text-black">
                                 <IoHeartDislike className="w-12 h-12" />
                             </button>
                         </div>
@@ -50,7 +76,13 @@ const Watched = () =>{
                 </div>
             ))}
             </div>
-    </div>
+            {showAlert && (
+            <Alert
+                message="An offer has been disliked"
+                onClose={() => setShowAlert(false)}
+            />
+            )}
+        </div>
        
         
     );
--- src/components/utilComponents/Alert.js
+++ src/components/utilComponents/Alert.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const Alert = ({message, onClose}) => {
 
-    return (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
-            <div className="bg-white border border-gray-300 rounded-md shadow-lg p-4 max-w-sm w-full">
+   return (
+        <div className="fixed bottom-0 right-0 m-8">
+            <div className="bg-white  rounded-md shadow-lg p-4 max-w-sm w-full">
                 <div className="flex justify-between items-center">
-                <p className="text-lg font-semibold">{message}</p>
-                <button
-                    onClick={onClose}
-                    className="text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
-                >
-                    <svg
-                    className="w-4 h-4 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
+                    <p className="text-lg font-semibold pr-2">{message}</p>
+                    <button
+                        onClick={onClose}
+                        className="text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 p-2"
                     >
-                    <path
-                        fillRule="evenodd"
-                        d="M18.293 1.293a1 1 0 0 0-1.414 0L10 8.586 2.707 1.293A1 1 0 1 0 1.293 2.707L8.586 10l-7.293 7.293a1 1 0 1 0 1.414 1.414L10 11.414l7.293 7.293a1 1 0 0 0 1.414-1.414L11.414 10l7.293-7.293a1 1 0 0 0 0-1.414z"
-                    />
-                    </svg>
-                </button>
+                        <svg
+                            className="w-4 h-4 fill-current"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M18.293 1.293a1 1 0 0 0-1.414 0L10 8.586 2.707 1.293A1 1 0 1 0 1.293 2.707L8.586 10l-7.293 7.293a1 1 0 1 0 1.414 1.414L10 11.414l7.293 7.293a1 1 0 0 0 1.414-1.414L11.414 10l7.293-7.293a1 1 0 0 0 0-1.414z"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
